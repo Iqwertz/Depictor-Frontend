f612fefc7c46ed5eb77d8f38d9fb8fb05844a820
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2021-10-28 23:48:15 +0200

    mistake

--- SelfieDrawer/src/app/app.module.ts
+++ SelfieDrawer/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TakeSelfieComponent } from './sites/take-selfie/take-selfie.component';
 import { GcodeComponent } from './sites/gcode/gcode.component';
-import { CameraWindowComponent } from './components/camera-window/camera-window.component';
+import { CameraWindowComponent } from './components/take-selfie-components/camera-window/camera-window.component';
 import { WebcamModule } from 'ngx-webcam';
-import { CameraTriggerComponent } from './components/camera-trigger/camera-trigger.component';
+import { CameraTriggerComponent } from './components/take-selfie-components/camera-trigger/camera-trigger.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OpenCameraButtonComponent } from './components/open-camera-button/open-camera-button.component';
-import { FlashComponent } from './components/flash/flash.component';
-import { SelfieDisplayComponent } from './components/selfie-display/selfie-display.component';
-import { RetakeSelfieButtonComponent } from './components/retake-selfie-button/retake-selfie-button.component';
-import { SubmitSelfieComponent } from './components/submit-selfie/submit-selfie.component';
+import { OpenCameraButtonComponent } from './components/take-selfie-components/open-camera-button/open-camera-button.component';
+import { FlashComponent } from './components/take-selfie-components/flash/flash.component';
+import { SelfieDisplayComponent } from './components/take-selfie-components/selfie-display/selfie-display.component';
+import { RetakeSelfieButtonComponent } from './components/take-selfie-components/retake-selfie-button/retake-selfie-button.component';
+import { SubmitSelfieComponent } from './components/take-selfie-components/submit-selfie/submit-selfie.component';
 
 @NgModule({
   declarations: [
