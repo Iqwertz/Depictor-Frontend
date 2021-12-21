cc3738b7098a8ac5f5351b369d87d12cf408314d
Julius <juliushussl@gmail.com>
2021-12-22 00:58:46 +0100

    gallery more

--- SelfieDrawer/src/environments/environment.ts
+++ SelfieDrawer/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   ip: '192.168.0.53:3001', //'localhost:3001',
   endGcode: 'M05;\nG01X0Y0;',
-  appStateCheckInterval: 2000,
+  appStateCheckInterval: 2500,
   useCameraAPI: false,
 };
 
