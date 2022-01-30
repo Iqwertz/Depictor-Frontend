a1f417159bdd3ca9aa8609cf7d33013a8231ff18
Julius <juliushussl@gmail.com>
2022-01-30 12:47:25 +0100

    addedloader during image compression

--- SelfieDrawer/src/app/services/file-upload.service.ts
+++ SelfieDrawer/src/app/services/file-upload.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { CameraServiceService } from './camera-service.service';
 import imageCompression from 'browser-image-compression';
 import { environment } from '../../environments/environment';
+import { LoadingService } from '../modules/shared/services/loading.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileUploadService {
-  constructor(private cameraService: CameraServiceService) {}
+  constructor(
+    private cameraService: CameraServiceService,
+    private loadingService: LoadingService
+  ) {}
 
   parseImageUpload($event: any) {
     this.blobToBase64($event.target.files[0]).then(
       (result: string | ArrayBuffer | null) => {
         if (typeof result === 'string') {
+          this.loadingService.isLoading = true;
+          this.loadingService.loadingText = 'compressing image';
           imageCompression
             .getFilefromDataUrl(result, 'upload.jpg')
             .then((file: File) => {
@@ -22,6 +28,8 @@ export class FileUploadService {
                 imageCompression
                   .getDataUrlFromFile(file)
                   .then((b64: string) => {
+                    this.loadingService.isLoading = false;
+                    this.loadingService.loadingText = '';
                     this.setUploadedImage(b64);
                   });
               });
